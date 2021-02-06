dc2d8a7ba7c3667e34a19a628430a9aab6aae236
Diarrah <diarrahcasiano@gmail.com>
2021-02-06 11:48:45 -0800

    One universal function to determine winner no matter which mode

--- src/components/Battleground.js
+++ src/components/Battleground.js
@@ -2,53 +2,50 @@ import React, { useEffect, useContext, useState } from 'react';
 import { GameContext } from '../App';
 import { RetryBtn } from './';
 import { triangle } from '../images/battleground';
-import { rock, paper, scissors } from '../images/RPS';
-import { grizzly, cowboy, ninja } from '../images/GCN';
+import { red1, blue1, yellow1 } from '../images/GCN';
+import { red2, blue2, yellow2 } from '../images/RPS';
 
 const Battleground = () => {
-    const { grizzlyMode, rpsScore, setRpsScore, gcnScore, setGcnScore, playing, setPlaying, playerWeapon, setPlayerWeapon, weaponsRef } = useContext(GameContext);
-    const [computerWeapon, setComputerWeapon] = useState(),
-          [whoWon, setWhoWon] = useState();
+    const { 
+        grizzlyMode, 
+        rpsScore, setRpsScore, 
+        gcnScore, setGcnScore, 
+        playing, setPlaying, 
+        computerWeapon, setComputerWeapon,
+        weaponsRef } = useContext(GameContext);
+
+    const [whoWon, setWhoWon] = useState(),
+          [playerWeapon, setPlayerWeapon] = useState();
+
 
     useEffect(() => {
-        const haha = () => {
-            let weapons = Array.from(document.querySelectorAll('.battleground__weapon')),
-                rpsOptions = ['rock', 'paper', 'scissors'],
-                gcnOptions = ['grizzly', 'cowboy', 'ninja'],
-                randomNumber;
+        let weapons = Array.from(document.querySelectorAll('.battleground__weapon')),
+            options = ['red', 'blue', 'yellow'];
+        
+        weaponsRef.current = weapons;
 
-            (function(){
-                randomNumber = Math.ceil(Math.random() * 3) - 1
+        (function(){
+            let randomNumber = Math.ceil(Math.random() * 3) - 1
+            setComputerWeapon(options[randomNumber])
+        })()
 
-                if (grizzlyMode) setComputerWeapon(gcnOptions[randomNumber])
-                else if (!grizzlyMode) setComputerWeapon(rpsOptions[randomNumber]);
-            })()
-
-            weapons.forEach(weapon => weapon.addEventListener('click', (e) => {
-                yesTheGameIsPlaying(e);
-                setPlaying(true)
-            }))
-
-            weaponsRef.current = weapons;
-        }
-
-        haha() 
+        weapons.forEach(weapon => weapon.addEventListener('click', (e) => {
+            beginTheBattle(e);
+            setPlaying(true)
+        }))
     }, [])
 
+    
     useEffect(() => {
-        if (playing && playerWeapon) {
-            grizzlyMode 
-                ? determineWinnerGCN() 
-                : determineWinnerRPS()
-        }
-    }, [playerWeapon && playing])
+        playing && determineWinner()
+    }, [playing])
+ 
 
-
-    const yesTheGameIsPlaying = (e) => {
+    const beginTheBattle = (e) => {
         weaponsRef.current.forEach(option => {
             if (option === e.target) {
                 option.classList.add('playing') 
-                setPlayerWeapon(e.target.dataset.selection)
+                setPlayerWeapon({ color: e.target.dataset.color, selection: e.target.dataset.selection })
             } else {
                 option.classList.add('hidden')
             }
@@ -57,50 +54,48 @@ const Battleground = () => {
         })
     }
     
-
-    const determineWinnerGCN = () => {
-        if (computerWeapon === playerWeapon) setWhoWon(0)
+    const determineWinner = () => {
+        if (computerWeapon === playerWeapon.color) setWhoWon(0)
 
         else if (
-            computerWeapon === 'ninja' && playerWeapon === 'cowboy'
-            || computerWeapon === 'grizzly' && playerWeapon === 'ninja'
-            || computerWeapon === 'cowboy' && playerWeapon === 'grizzly'
+            computerWeapon === 'red' && playerWeapon.color === 'yellow'
+            || computerWeapon === 'blue' && playerWeapon.color === 'red'
+            || computerWeapon === 'yellow' && playerWeapon.color === 'blue'
         ) {
             setWhoWon(-1)
             setTimeout(() => {
-                setGcnScore(gcnScore - 1)
-                sessionStorage.setItem('gcn', Number(gcnScore - 1))
+                if (grizzlyMode) {
+                    setGcnScore(gcnScore - 1)
+                    sessionStorage.setItem('gcn', Number(gcnScore - 1))
+                } else {
+                    setRpsScore(rpsScore -1)
+                    sessionStorage.setItem('rps', Number(rpsScore - 1))
+                }
             }, 2500);
                         
         } else {
             setWhoWon(1)
             setTimeout(() => {
-                setGcnScore(gcnScore + 1)
-                sessionStorage.setItem('gcn', Number(gcnScore + 1))
+                if (grizzlyMode) {
+                    setGcnScore(gcnScore + 1)
+                    sessionStorage.setItem('gcn', Number(gcnScore + 1))
+                } else {
+                    setRpsScore(rpsScore + 1)
+                    sessionStorage.setItem('rps', Number(rpsScore + 1))
+                }
             }, 2500);
         }
     }
 
-    const determineWinnerRPS = () => {
-        if (computerWeapon === playerWeapon) setWhoWon(0)
-
-        else if (
-            computerWeapon === 'paper' && playerWeapon === 'rock'
-            || computerWeapon === 'rock' && playerWeapon === 'scissors'
-            || computerWeapon === 'scissors' && playerWeapon === 'paper'
-        ) {
-            setWhoWon(-1)
-            setTimeout(() => {
-                setRpsScore(rpsScore - 1)
-                sessionStorage.setItem('rps', Number(rpsScore - 1))
-            }, 2500);
-
-        } else { 
-            setWhoWon(1)
-            setTimeout(() => {
-                setRpsScore(rpsScore + 1)
-                sessionStorage.setItem('rps', Number(rpsScore + 1))
-            }, 2500);
+    const correctName = (weapon) => {
+        if (grizzlyMode) {
+            if (weapon === 'yellow') return 'cowboy'
+            else if (weapon === 'red') return 'ninja'
+            else return 'grizzly'
+        } else {
+            if (weapon === 'yellow') return 'scissors'
+            else if (weapon === 'red') return 'rock'
+            else return 'paper'
         }
     }
 
@@ -109,17 +104,20 @@ const Battleground = () => {
         <div className="battleground">
             <img className={`triangle ${playing ? 'hidden' : ''}`} src={triangle} alt="Triangle" />
             
-            <div className={`battleground__weapon ${grizzlyMode ? 'weapon--ninja' : 'weapon--rock'}`} data-selection={grizzlyMode ? 'ninja' : 'rock'}>
+            <div className="battleground__weapon weapon--red" 
+                 data-color="red" data-selection={grizzlyMode ? 'ninja' : 'rock'}>
                 <span />
-                <img src={grizzlyMode ? ninja : rock} alt={`${grizzlyMode ? 'Ninja' : 'Rock'} selection`} /> 
+                <img src={grizzlyMode ? red1 : red2} alt={`${grizzlyMode ? 'Ninja' : 'Rock'} selection`} /> 
             </div>
-            <div className={`battleground__weapon ${grizzlyMode ? 'weapon--bear' : 'weapon--paper'}`} data-selection={grizzlyMode ? 'grizzly' : 'paper'}>
+            <div className="battleground__weapon weapon--blue"
+                 data-color="blue" data-selection={grizzlyMode ? 'grizzly' : 'paper'}>
                 <span />
-                <img src={grizzlyMode ? grizzly : paper} alt={`${grizzlyMode ? 'Bear' : 'Paper'} selection`} />
+                <img src={grizzlyMode ? blue1 : blue2} alt={`${grizzlyMode ? 'Bear' : 'Paper'} selection`} />
             </div>
-            <div className={`battleground__weapon ${grizzlyMode ? 'weapon--cowboy' : 'weapon--scissors'}`}  data-selection={grizzlyMode ? 'cowboy' : 'scissors'}>
+            <div className="battleground__weapon weapon--yellow"  
+                 data-color="yellow" data-selection={grizzlyMode ? 'cowboy' : 'scissors'}>
                 <span />
-                <img src={grizzlyMode ? cowboy : scissors} alt={`${grizzlyMode ? 'Cowboy' : 'Scissors'} selection`} />
+                <img src={grizzlyMode ? yellow1 : yellow2} alt={`${grizzlyMode ? 'Cowboy' : 'Scissors'} selection`} />
             </div>  
 
             {playing && (
@@ -128,22 +126,22 @@ const Battleground = () => {
                 <svg className={`circle ${computerWeapon}`} width="130" height="130" viewBox="0 0 130 130" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M65 7.5C96.7564 7.5 122.5 33.2436 122.5 65C122.5 96.7564 96.7564 122.5 65 122.5C33.2436 122.5 7.5 96.7564 7.5 65C7.5 33.2436 33.2436 7.5 65 7.5Z" stroke="#E1E4ED" strokeWidth="12"/>
                 </svg>
-                    <img src={require(`../images/${grizzlyMode ? 'GCN' : 'RPS'}/icon-${computerWeapon}.svg`).default} alt={`${computerWeapon} selection`} /> 
+                    <img src={require(`../images/${grizzlyMode ? 'GCN' : 'RPS'}/icon-${computerWeapon}.svg`).default} alt={`${correctName(computerWeapon)} selection`} /> 
                 </div> 
 
                 <div className="battleground__result">
                 <h3>{whoWon === 0
                         ? `It's a tie!`
-                            : whoWon === 1
-                                ? `You win`
-                                : `You lose`
+                        : whoWon === 1
+                            ? `You win`
+                            : `You lose`
 
                     }</h3>
                     {whoWon !== 0 && (
                         <span>
                             {whoWon === 1
-                                ? `${playerWeapon} beats ${computerWeapon}`
-                                : `${computerWeapon} beats ${playerWeapon}`
+                                ? `${playerWeapon.selection} beats ${correctName(computerWeapon)}`
+                                : `${correctName(computerWeapon)} beats ${playerWeapon.selection}`
                             }
                         </span>
                     )}
