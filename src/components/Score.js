bb0c25e55a14fdd8c54c21c27d64cfa470d2f2af
Diarrah <diarrahcasiano@gmail.com>
2021-02-06 21:29:26 -0800

    Slider for game name based on game mode

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,24 +1,29 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { GameContext } from '../App';
+import { Reset } from '../components';
 
 const Score = () => {
-    const { rpsScore, setRpsScore, gcnScore, setGcnScore, grizzlyMode, transition } = useContext(GameContext);
+    const { rpsScore, gcnScore, grizzlyMode } = useContext(GameContext);
 
     return (
         <div className="score">
             <div className="score__textbox">
-                <div className="textbox__container">{grizzlyMode ? 'Grizzly Bear' : 'Rock'}
-                    <span className={transition ? 'active' : ''} />
-                </div>
-                <div className="textbox__container">{grizzlyMode ? 'Cowboy' : 'Paper'}
-                    <span className={transition ? 'active' : ''} />
-                </div>
-                <div className="textbox__container">{grizzlyMode ? 'Ninja' : 'Scissors'}
-                    <span className={transition ? 'active' : ''} />
+                <div className={`textbox__inner${!grizzlyMode ? ' active' : ''}`}>
+                    <div className="textbox__inner--grizzly">
+                        <span>Grizzly Bear</span><br />
+                        <span>Cowboy</span><br />
+                        <span>Ninja</span>
+                    </div>
+                    <div className="textbox__inner--scissors">
+                        <span>Rock</span><br />
+                        <span>Paper</span><br />
+                        <span>Scissors</span>
+                    </div>
                 </div>
             </div>
-            <div className="score__number">Score <br />
-                {grizzlyMode ? gcnScore : rpsScore}
+            <div className="score__number">Score 
+                <span>{grizzlyMode ? gcnScore : rpsScore}</span>
+                <Reset />
             </div>
         </div>
     )
