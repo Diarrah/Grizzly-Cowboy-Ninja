493e6f980058326074b2595a25b4ea299a244845
Diarrah <diarrahcasiano@gmail.com>
2021-02-06 21:26:26 -0800

    Changed to data-color name

--- src/images/GCN/index.js
+++ src/images/GCN/index.js
@@ -1,6 +1,6 @@
-export { default as bear } from './icon-bear.svg';
-export { default as cowboy } from './icon-cowboy.svg';
-export { default as ninja } from './icon-ninja.svg';
+export { default as blue1 } from './icon-blue.svg';
+export { default as yellow1 } from './icon-yellow.svg';
+export { default as red1 } from './icon-red.svg';
 export { default as gcnRules } from './image-rules.svg';
 export { default as rule2 } from './g-vs-c.svg';
 export { default as rule3 } from './g-vs-n.svg';
--- src/images/RPS/index.js
+++ src/images/RPS/index.js
@@ -1,4 +1,4 @@
-export { default as rock } from './icon-rock.svg';
-export { default as paper } from './icon-paper.svg';
-export { default as scissors } from './icon-scissors.svg';
+export { default as red2 } from './icon-red.svg';
+export { default as blue2 } from './icon-blue.svg';
+export { default as yellow2 } from './icon-yellow.svg';
 export { default as rpsRules } from './image-rules.svg';
