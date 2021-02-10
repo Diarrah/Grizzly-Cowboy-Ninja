1b6a1214dd3783f591c5a191c25866f7550a6fe7
Diarrah <diarrahcasiano@gmail.com>
2021-02-09 18:29:43 -0800

    Rule--1 for GCN paragraph styled for <hr />

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -16,7 +16,7 @@ const Rules = () => {
 
     return (
         <>
-        <button className="rules__btn" onClick={() => setModuleOpen(true)}>Rules</button>
+        <button className="rules__btn" onClick={() => setModuleOpen(!moduleOpen)}>Rules</button>
         <div className={`rules__wrapper${moduleOpen ? ' active': ''}`}>
             <div className="wrapper__header">
                 <span>Rules</span>
@@ -31,18 +31,28 @@ const Rules = () => {
                     <img src={rpsRules} alt="Rules of Rock, Paper, Scissors" />
                 </div>
             )}
-            {grizzlyMode && (
+            {grizzlyMode && moduleOpen && (
                 <div className="rules rules--gcn">
                     <Swiper
                         direction="horizontal"
                         pagination={{ clickable: true }}
                         keyboard={{ enabled: true }}
                         simulateTouch={false}
-                        allowTouchMove={true}
                         slidesPerView={1}
                     >
                         <SwiperSlide className="rule rule--1">
-                            Have you even played Grizzly, Cowboy, Ninja? It's a fun twist on the classic game of Rock, Paper, Scissors. Super easy to learn, too, ready?
+                            <p>
+                                Have you ever played <span>Grizzly, Cowboy, Ninja?</span>
+                                <small>(Yes, this is a real game!)</small> 
+                                    <br /><br /><hr /><br />
+                                It's a fun twist on the classic game of <span>Rock, Paper, Scissors.</span>
+                                    <br /><hr /><br />
+                                The rules are just as straight forward and easy to learn, too.<br /><br />
+                                    
+                                <svg className="arrow" width="201" height="8" viewBox="0 0 201 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M200.354 4.35355C200.549 4.15829 200.549 3.84171 200.354 3.64645L197.172 0.464466C196.976 0.269204 196.66 0.269204 196.464 0.464466C196.269 0.659728 196.269 0.976311 196.464 1.17157L199.293 4L196.464 6.82843C196.269 7.02369 196.269 7.34027 196.464 7.53553C196.66 7.7308 196.976 7.7308 197.172 7.53553L200.354 4.35355ZM0 4.5H200V3.5H0V4.5Z" fill="#3B4363"/>
+                                </svg>
+                            </p>
                         </SwiperSlide>
                         <SwiperSlide className="rule rule--2">
                             <div className="rule__img__container">
@@ -50,8 +60,8 @@ const Rules = () => {
                             </div>
                             <div className="rule__textbox">
                                 <span>Cowboy vs. Bear</span> <br />
-                                Cowboy can pull & shoot quicker than Bear can close the distance for a swipe.<br />
-                                Cowboy wins.
+                                <p>Cowboy can pull & shoot quicker than Bear can close the distance for a swipe.</p>
+                                <span>Cowboy wins.</span>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide className="rule rule--3">
@@ -60,8 +70,8 @@ const Rules = () => {
                                 </div>
                             <div className="rule__textbox">
                                 <span>Bear vs. Ninja</span><br />
-                                Ninja only has 1 sword but Bear has 10 - his claws! He is sliced up in no time.<br />
-                                Bear wins.
+                                <p>Ninja only has 1 sword but Bear has 10 - his claws! He is sliced up in no time.</p>
+                                <span>Bear wins.</span>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide className="rule rule--4">
@@ -70,8 +80,8 @@ const Rules = () => {
                             </div>
                             <div className="rule__textbox">
                                 <span>Ninja vs. Cowboy</span><br />
-                                Ninja is sly & can sneak up on Cowboy before he even has a chance to notice.<br />
-                                Ninja wins.
+                                <p>Ninja is sly & can sneak up on Cowboy before he even has a chance to notice.</p>
+                                <span>Ninja wins.</span>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide className="rule rule--5">
